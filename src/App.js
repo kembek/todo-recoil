612c4048203c64b2e39fdc27cf76f25dbd9d6e5c
Beknazar <zevs.312.777@gmail.com>
2022-05-22 15:54:40 +0600

    creating CharacterCounter component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { atom, useRecoilState, selector, useRecoilValue } from "recoil";
 
-function App() {
+const textState = atom({
+  key: "textState",
+  default: "",
+});
+
+const charCountState = selector({
+  key: "charCountState",
+  get: ({ get }) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+function CharacterCounter() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TextInput />
+      <CharacterCount />
     </div>
   );
+}
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <p>Character count: {count}</p>;
+}
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={handleChange} />
+      <br />
+      <p>Echo: {text}</p>
+    </div>
+  );
+}
+
+function App() {
+  return <CharacterCounter />;
 }
 
 export default App;
